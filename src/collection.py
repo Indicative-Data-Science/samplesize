c42c47db1277bfd99196825b6b0c74c6e2bb1fe5
TerryLines <terry.lines@gmail.com>
2021-07-13 09:01:11 +0100

    metrics

---
+++ src/collection.py
@@ -0,0 +1,62 @@
+from itertools import chain,repeat
+import gnssmapper as gm
+import geopandas as gpd
+import pandas as pd
+import numpy as np
+from pygeos.io import ShapelyGeometry
+import shapely.ops
+import shapely.geometry
+
+
+def create_obs(map_,bounds,start, end,samples,*args):
+    gm.geo.pyproj.network.set_network_enabled()
+    """ create a df of observations denoted by their x-y locations on ground and w-z locations on building """    
+    receiverpoints = gm.sim.point_process(map_,bounds,start, end,samples,*args)
+    obs = gm.observe(receiverpoints, ['G', 'R', 'C', 'E'])
+    obs = gm.geo.to_crs(obs, map_.crs)
+    buildings = chain(*repeat(map_.geometry, len(obs)))
+    points = list(gm.geo.intersection_projected(obs.geometry, buildings))
+    pointsarray = np.array([(np.nan,np.nan,np.nan) if p.is_empty else p.coords[0] for p in points])
+    a,b,z = pointsarray[:,0],pointsarray[:,1],pointsarray[:,2]
+    line = map_.geometry[0].exterior
+    w = np.array([np.nan if p.is_empty else line.project(p) for p in points])
+    xy = np.array([l.coords[0][0:2] for l in obs.geometry])
+    x,y= xy[:,0],xy[:,1]
+    obs_points=shapely.geometry.asMultiPoint(xy)
+    az = np.where(b>y,0,180)+np.rad2deg(np.arctan((a-x)/(b-y)))
+    az=np.mod(az,360)
+    d_ray = ((a-x)**2 +(b-y)**2)**0.5
+    d_building = np.array([p.distance(map_.geometry[0]) for p in obs_points])
+    return pd.DataFrame({'x':x,'y':y,'w':w,'z':z,'a':a,'b':b,'d_ray':d_ray,'d_building':d_building,'az':az,'el':gm.observations.elevation(obs.geometry)})
+    
+def create_image(x,y,i,resolution,bounds,mean=True):
+    minx,miny,maxx,maxy=bounds
+    mask = ~np.isnan(x) & ~np.isnan(y)
+    i_=i[mask]
+    x_=x[mask]
+    y_=y[mask]
+    points=pd.DataFrame()
+    points['x'] = np.floor(np.maximum((np.minimum(x_,maxx) - minx),0) * resolution).astype('int64')
+    points['y'] = np.floor(np.maximum((np.minimum(y_,maxy) - miny),0) * resolution).astype('int64')
+    points['i'] = i_
+    if mean:
+        pixels=points.groupby(['x','y']).i.mean().reset_index()
+    else:
+        pixels=points.groupby(['x','y']).i.sum().reset_index()
+    nrows=np.max(points.x)+1
+    ncols = np.max(points.y)+1
+    img = np.zeros((ncols,nrows))
+    img[:]=np.nan
+    img[ncols-1-pixels.y,pixels.x]=pixels.i
+    return img
+
+if __name__=="__main__":
+    import sys
+    samples=int(sys.argv[1])
+    writepath=sys.argv[2]
+    map_= gpd.GeoDataFrame({'height':[10],'geometry':[shapely.wkt.loads("POLYGON((528005 183005, 528005 182995,527995 182995, 527995 183005,528005 183005))")]},crs=7405)
+    box = shapely.geometry.box(527935, 182935, 528065,183065)
+    start = pd.Timestamp('2021-01-01 00:00:00')
+    end = pd.Timestamp('2021-01-01 23:59:59')
+    obs= create_obs(map_,box.bounds,start,end,samples)
+    obs.to_csv(writepath)
--- src/metrics.py
+++ src/metrics.py
@@ -1,16 +1,11 @@
-from itertools import chain, repeat
-import gnssmapper as gm
-import geopandas as gpd
 import pandas as pd
 import numpy as np
-import shapely
-
+import gnssmapper as gm
 
 def _indicator(x, height, distance=np.inf):
     if distance == np.inf:
         return ~np.isnan(x)
     return ((height-distance < x) & (x < height+distance))
-
 
 def int_metric(x, height, distance=np.inf):
     """ Intersection metric for intersection points. Number of points within a given distance of height.
@@ -36,30 +31,57 @@ def window_metric(x, height, n):
     else:
         return np.sort(distances)[n-1]
 
+def generate_samples(data,height):
+    int_d= [2**i for i in range(5)]
+    samples=[]
+    for d,out_d in zip(int_d,int_d[1:]+[np.inf]):
+        in_obs = _indicator(data.height,height,d)
+        out_obs = _indicator(data.height,height,out_d) & ~in_obs
+        in_idx=np.flatnonzero(in_obs)
+        out_idx=np.flatnonzero(out_obs)
+        max_size=np.sum(in_obs)+np.sum(out_obs)
+        sample_size=[2**i for i in range(3,np.ceil(np.log2(max_size)).astype('int'))]
+        insample_proportion=np.arange(0.1,1.1,0.1)
+        reps=range(10)
+        insample_size = (np.floor(p*s).astype('int') for s in sample_size for p in insample_proportion)
+        outsample_size = (np.ceil((1-p)*s).astype('int') for s in sample_size for p in insample_proportion)
+        for i,o in zip(insample_size,outsample_size):
+            print(f'indicator {d},insample {i}, outsample {o}')
+            for _ in reps:
+                in_idx_sample=np.random.choice(in_idx,size=min(i,in_idx.shape[0]),replace=False)
+                out_idx_sample=np.random.choice(out_idx,size=min(o,out_idx.shape[0]),replace=False)
+                idx_sample= np.concatenate((in_idx_sample,out_idx_sample))
+                sample=np.zeros_like(in_obs)
+                sample[idx_sample]=True
+                samples.append(sample)
+    out = np.stack(samples,axis=-1)
+    print ('returning output')
+    return out
+
+def run_sample(data,sample_array,height):
+    int_d=[2**i for i in range(5)]+[np.inf]
+    window_n=[10**i for i in range(2,6)]
+    colnames= ["I_"+str(d) for d in int_d]+["W_"+str(n) for n in window_n]+['est_lb','est','est_ub']
+    results=[]
+    for i,sample in enumerate(sample_array.T):
+        if i%100==0:
+            print(i/sample_array.shape[1])
+        h=data.height[sample].values
+        ss=data.SS[sample].values
+        metrics=[int_metric(h,height,d) for d in int_d]+[window_metric(h,height,n) for n in window_n]
+        est=gm.algo.fit_edge(h,ss,np.array([40,0.1,30,10]))
+        results.append(np.array(metrics+list(est)))
+    out=np.stack(results,axis=0)
+    out=pd.DataFrame(out,columns=colnames)
+    return(out)
+        
 
 if __name__=="__main__":
     import sys
     data=pd.read_csv(sys.argv[1])
-    true_height=pd.read.csv
-
-    def generate_datasets(filepath,height):
-        data=pd.read_csv(filepath)
-        data=data[_indicator(data.height,height,np.inf),]
-        max_size=data.shape[0]
-        windows= [2**i for i in range(6)]
-        counts= [10**i for i in range(1,6)]
-        int_indicators = [_indicator(data.height,height,d) for d in windows]
-        window_indicators = [_indicator(data.height,height,window_metric(data.height,height,c)) for c in counts]
-        indicators=int_indicators.extend(window_indicators)
-        for w in windows:
-            indicator = _indicator(data.height,height,w)
-            insample_sizes = [2**i for i in range(5,np.floor(np.log2(np.sum(indicator)))]
-            for i in insample_sizes:
-                outsample
-                outsample_sizes = insample_sizes[2**i for i in range(np.sum(~indicators)
-            insample_sizes 
-
-
-    sample_size=[2**i for i in range]
-
-    for _ in range()
+    height=int(sys.argv[2])
+    writepath=sys.argv[3]
+    samples=generate_samples(data,height)
+    np.save(writepath,samples)
+    results=run_sample(data,samples,height)
+    results.to_csv(writepath+".csv")
